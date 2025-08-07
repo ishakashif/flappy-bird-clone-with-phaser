040dfd7fcf4218b342382318e29c6a1cb4283fcb
Isha Kashif <ishak@udel.edu>
2025-08-07 18:42:05 -0400

    objects

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-function preload () {
 
-}
+let config = {
+    renderer: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y:300},
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+  }; 
 
-function create () {
+  let game = new Phase.Game(config);
 
-}
 
-function update () {
-    
-}
+
+  function preload () {
+
+  }
+
+  function create () {
+
+  }
+
+  function update () {
+
+  }
