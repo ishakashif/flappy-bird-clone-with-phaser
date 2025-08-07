c8fab528ad993ee92a4ddf918d33d4e11bf067d6
Isha Kashif <ishak@udel.edu>
2025-08-07 18:51:27 -0400

    updated the create function

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ let config = {
 
   function create () {
     const background = this.add.image(0, 0, 'background').setOrigin(0,0);
+    const roads = this.physics.add.staticGroup();
+    const road = roads.create(400, 568, 'road').setScale(2).refreshBody();
 
   }
 
