d5edf925a4f3cfd1c1a454d34fb5e9de32c5faaf
Isha Kashif <ishak@udel.edu>
2025-08-08 12:50:20 -0400

    bird now falls on top of road rather than through it

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ let config = {
     const road = roads.create(400, 568, 'road').setScale(2).refreshBody(); 
     bird = this.physics.add.sprite(0, 50, 'bird').setScale(2);
     bird.setBounce(0.2);
-    bird.setCollideWorldBounds(true);
+    bird.setCollideWorldBounds(true); 
+    this.physics.add.collider(bird, road);
   }
 
   function update () {
